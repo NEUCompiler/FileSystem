690c756632c7d9d30166c319f93edb46e17a37eb
刘鑫伟 <liuxinwei1995@gmail.com>
2016-07-04 19:22:07 +0800

    框架。

---
+++ FileSystem/src/com/fileSystem/operation/Operation.java
@@ -0,0 +1,94 @@
+package com.fileSystem.operation;
+
+/**
+ * 基本操作。
+ * 
+ * @author 刘鑫伟
+ *
+ */
+public class Operation {
+
+	/**
+	 * 文件创建。
+	 */
+	public void create() {
+		
+	}
+	
+	/**
+	 * 文件删除。
+	 */
+	public void delete() {
+		
+	}
+	
+	/**
+	 * 文件读取。
+	 */
+	public void read() {
+		
+	}
+	
+	/**
+	 * 文件写入。
+	 */
+	public void write() {
+		
+	}
+	
+	/**
+	 * 文件打开。
+	 */
+	public void open() {
+		
+	}
+	
+	/**
+	 * 文件关闭。
+	 */
+	public void close() {
+		
+	}
+	
+	/**
+	 * 创建目录。
+	 */
+	public void mkdir() {
+		
+	}
+	
+	/**
+	 * 改变目录。
+	 */
+	public void chdir() {
+		
+	}
+	
+	/**
+	 * 列出文件目录。
+	 */
+	public void dir() {
+		
+	}
+	
+	/**
+	 * 登陆。
+	 */
+	public void login() {
+		
+	}
+	
+	/**
+	 * 退出。
+	 */
+	public void logout() {
+		
+	}
+	
+	/**
+	 * 格式化。
+	 */
+	public void format() {
+		
+	}
+}
